142b2e6b1db2f69ab1e99f3745e4b3042a7ef20b
Gergely Imreh <imrehg@gmail.com>
2012-02-08 13:50:59 +0800

    more extended test init

--- web.js
+++ web.js
@@ -1,9 +1,32 @@
+require.paths.unshift(__dirname + '/lib');
+
 var express = require('express');
+var uuid = require('node-uuid');
 
 var app = express.createServer(express.logger());
 
+// create a socket.io backend for sending facebook graph data
+// to the browser as we receive it
+var io = require('socket.io').listen(app);
+
+// use xhr-polling as the transport for socket.io
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+  var method = request.headers['x-forwarded-proto'] || 'http';
+  var socket_id = uuid();
+
+  response.render('home.ejs', {
+          layout:   false,
+          app:      app,
+          home:     method + '://' + request.headers.host + '/',
+          redirect: method + '://' + request.headers.host + request.url,
+          socket_id: socket_id
+        });
 });
 
 var port = process.env.PORT || 3000;
